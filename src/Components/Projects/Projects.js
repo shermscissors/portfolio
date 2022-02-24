200a433b7674c366e4cc9f97ece0a2fda785ee07
shermscissors <christian.gales09@gmail.com>
2022-02-24 11:45:25 -0500

    Updating Mobile Styling

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,22 +11,37 @@ function Projects(props) {
 			{/* Creates DIV so that it can have a GRID DISPLAY inside of a FLEX container */}
 			<div id='projects-gallery'>
 				{/* Each individual panel */}
-				<div id='project-1'>
+				<div className='projects-grid-template' id='project-1'>
 					<h2 className='projects-subheading'>Project One</h2>
 					<img
-						src='/src/assets/project1.png'
+						className='project-photo'
+						src='/public/assets/project1.png'
 						alt='Project One: Self-Scoring Photography Trivia'
-						loading='Loading...'
 					/>
 				</div>
-				<div id='project-2'>
+				<div className='projects-grid-template' id='project-2'>
 					<h2 className='projects-subheading'>Project Two</h2>
+					<img
+						className='project-photo'
+						src='/public/assets/project2.png'
+						alt='Project Two: Just Weather'
+					/>
 				</div>
-				<div id='project-3'>
+				<div className='projects-grid-template' id='project-3'>
 					<h2 className='projects-subheading'>Project Three</h2>
+					<img
+						className='project-photo'
+						src='/public/assets/project3.png'
+						alt='Project Three: The Watson'
+					/>
 				</div>
-				<div id='project-4'>
+				<div className='projects-grid-template' id='project-4'>
 					<h2 className='projects-subheading'>Project Four</h2>
+					<img
+						className='project-photo'
+						src='/public/assets/project4.png'
+						alt='Project Four: PeeP'
+					/>
 				</div>
 			</div>
 		</div>
