6ec22b2a700f8c6e419353d4a70f30aefadeb508
shermscissors <christian.gales09@gmail.com>
2022-02-24 13:36:24 -0500

    Animations Etc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import HeaderCompnent from './Components/Header/HeaderComponent';
 import Projects from './Components/Projects/Projects';
 
 function App() {
+		setTimeout(() => {
+			console.log(
+				"Welcome to the portfolio of David Gales: Software Developer. If you're reading this: you are either checking my code for a review, or you know about the developer tools! Either way, I think that's pretty neat. I want to put something special here for all the people that see this in th future. I don't quite know what to put yet. Maybe it'll be just this message alone..."
+			);
+		}, 5000);
 	return (
 		<div className='App'>
 			<header>
--- src/Components/Header/HeaderComponent.js
+++ src/Components/Header/HeaderComponent.js
@@ -1,15 +1,17 @@
-import './HeaderComponent.module.css';
+import './HeaderComponent.css';
 import React from 'react';
 
 function HeaderCompnent(props) {
 	return (
 		<div className='masthead'>
-            <h1 className='headings' id='masthead-h1'>Will this make me whole?</h1>
+			<h1 className='headings' id='masthead-h1'>
+				Will this make me whole?
+			</h1>
 			<a
 				href='https://docs.google.com/document/d/18npLLVlZKowKMP7ZYT4Ub3_Ma27Hxq6JN2MlGKXIvgw/edit?usp=sharing'
 				target={'_blank'}
 				rel='noreferrer'>
-				<button  id='masthead-button'>My Resume</button>
+				<button id='masthead-button'>My Resume</button>
 			</a>
 		</div>
 	);
